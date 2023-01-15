6019cdd5679c5f5b669259334994cf19ffeb1b41
Robin Ruud Kristensen <robin1998@hotmail.no>
2023-01-15 15:42:01 +0100

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let array = [];
+    for(let i = text.length-1; i >= 0; i--){
+        array.push(text[i]);
+    }
+    return array.join("");
 };
 
 // Do not edit below this line
