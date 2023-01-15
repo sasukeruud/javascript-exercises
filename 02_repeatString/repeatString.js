4cf392810e89e3f289ad7c4da46213a9abd87f41
Robin Ruud Kristensen <robin1998@hotmail.no>
2023-01-15 15:36:23 +0100

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    if(number < 0){
+        return "ERROR"
+    }
+    let array = [];
+    for(let i = 0; i < number; i++){
+        array.push(text);
+    }
+    return array.join("");
 };
 
 // Do not edit below this line
