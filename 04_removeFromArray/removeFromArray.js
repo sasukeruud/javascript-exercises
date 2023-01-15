6c5f9028d527def70a9761b285150999bd64993e
Robin Ruud Kristensen <robin1998@hotmail.no>
2023-01-15 15:59:15 +0100

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, remove1, remove2,remove3,remove4) {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === remove1 || array[i] === remove2 || array[i] === remove3
+            || array[i] === remove4){
+            continue;
+        }
+        newArray.push(array[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
