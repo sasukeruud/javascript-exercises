b8d3861435945b35b7b822e676af8a465f418937
Robin Ruud Kristensen <robin1998@hotmail.no>
2023-01-15 16:17:49 +0100

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let num = 0;
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
 
+    if(typeof(num1) != typeof(1) || typeof(num2) != typeof(1)){
+        return "ERROR";
+    }
+
+    if(num1 > num2){
+        for(let i = num2; i <= num1; i++){
+            num = num + i;
+        }
+    }else {
+        for(let i = num1; i <= num2; i++){
+            num = num + i;
+        }
+    }
+
+    return num;
 };
 
 // Do not edit below this line
